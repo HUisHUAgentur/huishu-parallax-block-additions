52c31486ca95cc3432183d48854d7522d06fd1c6
HUisHU-Sebastian <58956016+HUisHU-Sebastian@users.noreply.github.com>
2023-08-29 15:35:23 +0200

    Update huishu-parallax-block-additions.php

--- huishu-parallax-block-additions.php
+++ huishu-parallax-block-additions.php
@@ -5,7 +5,7 @@
  * Version:           1.0.1
  * Requires at least: 5.8
  * Requires PHP:      7.3
- * Author:            HUisHU. Digitale Kreativagentur.
+ * Author:            HUisHU. Digitale Kreativagentur GmbH
  * Author URI:        https://www.huishu-agentur.de
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
